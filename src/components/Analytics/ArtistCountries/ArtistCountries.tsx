94bd669e46eab39f3c34f5b04b49e8dd84f04fcb
Joshua <joshua@routeique.com>
2020-11-06 08:33:50 -0700

    Added countries data to properly render Choropleth

--- src/components/Analytics/ArtistCountries/ArtistCountries.tsx
+++ src/components/Analytics/ArtistCountries/ArtistCountries.tsx
@@ -1,7 +1,6 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {ResponsiveChoropleth} from '@nivo/geo';
 import artistCountriesFeatures from './artistCountriesFeatures';
-import artistCountriesData from './artistCountriesData';
 import './ArtistCountries.scss';
 import {Container} from "rsuite";
 import SpotifyApi from "../../../api/SpotifyApi";
@@ -9,8 +8,10 @@ import SpotifyContext from "../../../context/spotify";
 import useAsyncEffect from "../../../hooks/useAsyncEffect";
 import {alpha2to3} from 'iso3166-alpha-converter'
 import axios from 'axios';
+import {CacheKey} from "../../../utils/constants";
+import * as _ from 'lodash';
 
-const mb = require('musicbrainz');
+const cache = require('localstorage-ttl');
 
 interface ArtistCountry {
   id: string;
@@ -18,30 +19,65 @@ interface ArtistCountry {
 }
 
 const ArtistCountries = () => {
-  const [artistCountries, setArtistCountries] = useState<string[]>([]);
+  const [artistCountriesData, setArtistCountriesData] = useState<ArtistCountry[]>([]);
+  const [totalArtistCount, setTotalArtistCount] = useState(0);
   const {spotifyContext} = useContext(SpotifyContext);
 
   useAsyncEffect(async () => {
-    // console.log('canada', alpha2to3['ca']);
+    const artistCountriesDataCache: ArtistCountry[] = cache.get(CacheKey.ARTIST_COUNTRIES_DATA);
     const spotifyApi = new SpotifyApi(spotifyContext.accessToken);
 
-    const allFollowedArtists: any[] = await spotifyApi.getCurrentUserAllFollowedArtists();
-    allFollowedArtists.forEach((artist) => {
-    });
-  }, []);
+    const totalArtists = await spotifyApi.getTotalArtistCount(1);
+    setTotalArtistCount(totalArtists);
 
-  useEffect(() => {
-    // console.log('followedArtists', JSON.stringify(followedArtists));
-  }, [artistCountries]);
+    if (!artistCountriesDataCache) {
+      const promises: Promise<any>[] = [];
+
+      const allFollowedArtists: any[] = await spotifyApi.getCurrentUserAllFollowedArtists();
+      allFollowedArtists.forEach((artist) => {
+        promises.push(axios.get(`https://www.theaudiodb.com/api/v1/json/523532/search.php?s=${artist.name}`, {
+          headers: null,
+        }));
+      });
+
+      axios.all(promises).then(axios.spread((...responses: any) => {
+        let artistCountries: { name: string, country: string }[] = [];
+
+        responses.forEach((response: any) => {
+          if (response.data.artists !== null) {
+            artistCountries.push({
+              name: 'test',
+              country: alpha2to3[response.data.artists[0].strCountryCode.toLowerCase()] ?? 'N/A',
+            });
+          }
+        });
+
+        const artistCountriesNew: ArtistCountry[] = [];
+        const countries = _.groupBy(artistCountries, 'country');
+        _.forEach(countries, (value: any, key: any) => {
+          artistCountriesNew.push({ id: key, value: value.length });
+        });
+        setArtistCountriesData(artistCountriesNew);
+        cache.set(CacheKey.ARTIST_COUNTRIES_DATA, artistCountriesNew, 86400000);
+      }));
+    } else {
+      setArtistCountriesData(artistCountriesDataCache);
+    }
+  }, []);
 
   return (
     <Container className="analytics-container">
+      <div style={{textAlign: 'center', margin: '15px 0 30px 0'}}>
+        <h3>Artist's Countries</h3>
+        <h5>Displays which countries have the most artists based on the artists you follow.</h5>
+      </div>
+
       <ResponsiveChoropleth
         data={artistCountriesData}
         features={artistCountriesFeatures}
         margin={{top: 0, right: 0, bottom: 0, left: 0}}
         colors="spectral"
-        domain={[0, 1000000]}
+        domain={[0, totalArtistCount]}
         unknownColor="#666666"
         label="properties.name"
         valueFormat=".2s"
@@ -75,6 +111,21 @@ const ArtistCountries = () => {
             ]
           }
         ]}
+        tooltip={({ feature }: any) => {
+          const { label, data } = feature;
+
+          return (
+            <div style={{
+              color: 'black',
+              background: 'white',
+              border: '1px solid black',
+              borderRadius: '3px',
+              padding: '5px 8px',
+            }}>
+              {data && <>{label}: {data.value} {data.value > 1 ? 'Artists' : 'Artist'}</>}
+            </div>
+          );
+        }}
       />
     </Container>
   );
--- src/hooks/useAsyncEffect.ts
+++ src/hooks/useAsyncEffect.ts
@@ -1,6 +1,10 @@
 import { DependencyList, useEffect } from 'react';
 
 // Wrapper for useEffect hook to allow async/await to be used by changing the effect type to any
-const useAsyncEffect = (effect: () => any, deps?: DependencyList) => useEffect(effect, deps);
+const useAsyncEffect = (effect: () => any, deps?: DependencyList) => {
+  useEffect(() => {
+    effect();
+  }, deps);
+}
 
 export default useAsyncEffect;
