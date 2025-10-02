b927849f183746a452c227564cfc3e1d9af7a958
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-01 19:42:17 -0600

    Comment out unused imports for jwt and axios in index.tsx to clean up the code.

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import jwt from "jsonwebtoken";
-import axios from "axios";
+// import jwt from "jsonwebtoken";
+// import axios from "axios";
 
-const token = jwt.sign(
-  {},
-  import.meta.env.VITE_API_SERVER_JWT_TOKEN_SECRET ?? ""
-);
-axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
+// const token = jwt.sign(
+//   {},
+//   import.meta.env.VITE_API_SERVER_JWT_TOKEN_SECRET ?? ""
+// );
+// axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
 
 ReactDOM.render(
   <React.StrictMode>
